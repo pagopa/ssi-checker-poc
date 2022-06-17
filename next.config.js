0f26d237c2c81d92bd51fa7ca819398ae05fcbba
Mario Perrotta <mario.perrotta@pagopa.it>
2022-06-17 10:49:33 +0200

    chore: added image loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ""
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  images: {
+    loader: 'vercel',
+    path: '/'
+  }
 };
 
 module.exports = nextConfig
