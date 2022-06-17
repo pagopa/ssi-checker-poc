603ebc8ff9a04ee3c99d206b919a54b880148caf
Mario Perrotta <mario.perrotta@pagopa.it>
2022-06-17 10:50:53 +0200

    chore: replace loader with default

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
-    loader: 'vercel',
+    loader: 'default',
     path: '/'
   }
 };
