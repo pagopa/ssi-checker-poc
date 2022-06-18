c541fc13ada81480fe353ecedb15da31fde6409a
Mario Perrotta <mario.perrotta@pagopa.it>
2022-06-18 19:54:39 +0200

    fix: linter error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import qrcode from '../assets/qr-code.png';
 import { useRouter } from 'next/router';
