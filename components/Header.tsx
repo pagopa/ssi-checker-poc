56d3f0072aa52bfd43613bd9de96123f3ccab9c0
Mario Perrotta <mario.perrotta@pagopa.it>
2022-06-18 12:43:28 +0200

    refactor: styles improved

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 const Header = () => {
   const router = useRouter();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import type { NextPage } from "next";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-import qrcode from "../assets/qr-code.png";
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+import qrcode from '../assets/qr-code.png';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const router = useRouter();
   return (
     <div className={styles.container}>
       <style global jsx>{`
@@ -17,23 +19,26 @@ const Home: NextPage = () => {
         }
       `}</style>
       <div className={styles.top}>
-        <p>ID Checker</p>
-        <p>Ciao Marta</p>
+        <p className={styles.title}>ID Checker</p>
+        <p className={styles.username}>Ciao Marta</p>
         <p>
           ID Checker permette di verificare le Identità digitali dei cittadini
           in modo sicuro e veloce.
         </p>
       </div>
       <div className={styles.floatDiv}>
-        <Image src={qrcode} width="24" height="24" alt={"qrcode icon"} />
-        <p>Verifica un’identità digitale</p>
+        <Image src={qrcode} width="24" height="24" alt={'qrcode icon'} />
+        <p className={styles.boxTitle}>Verifica un’identità digitale</p>
         <p>
           Inquadra il Codice QR dell’identità digitale che vuoi verificare
           presente su IO Wallet del cittadino.
         </p>
-        <Link href="/reader" replace>
-          <a>Inquadra Codice QR</a>
-        </Link>
+        <button
+          className={styles.btnChecker}
+          onClick={() => router.push('/reader')}
+        >
+          Inquadra Codice QR
+        </button>
       </div>
     </div>
   );
