ac96472615c583ddc9cd0393dd6dfec56e787814
Mario Perrotta <mario.perrotta@pagopa.it>
2022-06-25 19:14:28 +0200

    fix: apple icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,11 @@ function MyDocument() {
     <Html>
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/assets/icons/icon-96x96.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="96x96"
+          href="/assets/icons/icon-96x96.png"
+        />
         <meta name="apple-mobile-web-app-status-bar" content="#1976d2" />
         <meta name="theme-color" content="#fff" />
       </Head>
