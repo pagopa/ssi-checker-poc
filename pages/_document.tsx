62aa4cc3d0b086802dd7f2c5de62b9848a0ff53e
Mario Perrotta <mario.perrotta@pagopa.it>
2022-06-25 19:02:39 +0200

    fix: added document to use manifest

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+function MyDocument() {
+  return (
+    <Html>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#fff" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default MyDocument;
